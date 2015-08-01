2c714fca35c6a4efd973595c3a27e4a6b6d52a32
NancyKo <konancys@gmail.com>
2015-08-01 10:27:33 -0400

    Finished

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def roll_call_dwarves(names)# code an argument here
   # Your code here
   names.each.with_index do |n, i|
@@ -11,13 +12,23 @@ def summon_captain_planet(calls)# code an argument here
   calls.collect {|n| n.capitalize + "!"} 
 end
 
-def long_planteer_calls# code an argument here
+def long_planteer_calls(array_of_calls)# code an argument here
   # Your code here
+  array_of_calls.any? {|a| a.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array_of_items)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses = []
+  array_of_items.each do |item|
+    cheeses << cheese_types.find { |cheese| cheese == item }
+  end
+    return cheeses.compact.first
+  # require 'pry'
+  # binding.pry
 end
+# array = ["tomato soup", "oyster crackers", "gouda"]
+# find_the_cheese(array)
 
-
+# ["tomato soup", "cheddar", "oyster crackers", "gouda"].find {|item| item == "gouda"}
